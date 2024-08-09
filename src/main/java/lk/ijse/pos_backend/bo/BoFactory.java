0292501b2eeff88ef5c8b65191b8803a34e3215b
AshanNaveen <ashannvn@gmail.com>
2024-08-09 07:32:18 +0530

    BO Factory initiated

--- src/main/java/lk/ijse/pos_backend/bo/BoFactory.java
+++ src/main/java/lk/ijse/pos_backend/bo/BoFactory.java
@@ -1,4 +1,33 @@
 package lk.ijse.pos_backend.bo;
 
+import lk.ijse.pos_backend.bo.custom.impl.CustomerBoImpl;
+import lk.ijse.pos_backend.bo.custom.impl.ItemBoImpl;
+import lk.ijse.pos_backend.bo.custom.impl.OrderBoImpl;
+
 public class BoFactory {
+    private static BoFactory boFactory;
+
+    private BoFactory() {
+    }
+
+    public static BoFactory getInstance() {
+        return (boFactory == null) ? (boFactory = new BoFactory()) : (boFactory);
+    }
+
+    public enum BoType {
+        CUSTOMER, ITEM, ORDER
+    }
+
+    public <T> T getBo(BoType boType) {
+        switch (boType) {
+            case CUSTOMER:
+                return (T) new CustomerBoImpl();
+            case ITEM:
+                return (T) new ItemBoImpl();
+            case ORDER:
+                return (T) new OrderBoImpl();
+            default:
+                return null;
+        }
+    }
 }
