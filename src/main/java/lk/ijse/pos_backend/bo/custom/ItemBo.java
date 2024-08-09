24431e7095b6e2f2cd94e0b5696cb2953dda19b9
AshanNaveen <ashannvn@gmail.com>
2024-08-09 07:33:35 +0530

    Item Bo Implemented with Methods

--- src/main/java/lk/ijse/pos_backend/bo/custom/ItemBo.java
+++ src/main/java/lk/ijse/pos_backend/bo/custom/ItemBo.java
@@ -1,4 +1,15 @@
 package lk.ijse.pos_backend.bo.custom;
 
+import lk.ijse.pos_backend.dto.ItemDTO;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ItemBo {
+    ItemDTO getItem(String id) throws SQLException;
+    List<ItemDTO> getAllItems() throws SQLException;
+    boolean saveItem(ItemDTO dto) throws SQLException;
+    boolean updateItem(ItemDTO dto) throws SQLException;
+    boolean deleteItem(String id) throws SQLException;
+    String generateNextId() throws SQLException;
 }
--- src/main/java/lk/ijse/pos_backend/bo/custom/impl/ItemBoImpl.java
+++ src/main/java/lk/ijse/pos_backend/bo/custom/impl/ItemBoImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.pos_backend.bo.custom.impl;
 
-public class ItemBoImpl {
+import lk.ijse.pos_backend.bo.custom.ItemBo;
+import lk.ijse.pos_backend.dao.custom.ItemDAO;
+import lk.ijse.pos_backend.dao.custom.impl.ItemDAOImpl;
+import lk.ijse.pos_backend.dto.ItemDTO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ItemBoImpl implements ItemBo {
+
+    ItemDAO itemDAO = new ItemDAOImpl();
+
+    @Override
+    public ItemDTO getItem(String id) throws SQLException {
+        return itemDAO.getById(id);
+    }
+
+    @Override
+    public List<ItemDTO> getAllItems() throws SQLException {
+        return itemDAO.getAll();
+    }
+
+    @Override
+    public boolean saveItem(ItemDTO dto) throws SQLException {
+        return itemDAO.save(dto);
+    }
+
+    @Override
+    public boolean updateItem(ItemDTO dto) throws SQLException {
+        return itemDAO.update(dto);
+    }
+
+    @Override
+    public boolean deleteItem(String id) throws SQLException {
+        return itemDAO.delete(id);
+    }
+
+    @Override
+    public String generateNextId() throws SQLException {
+        return itemDAO.generateNextId();
+    }
 }
