0074750bbb02c4adab034b0ae127a41f6548a7e9
AshanNaveen <ashannvn@gmail.com>
2024-08-09 07:34:10 +0530

    Item DAO and Item DTO implemented

--- src/main/java/lk/ijse/pos_backend/dao/custom/impl/ItemDAOImpl.java
+++ src/main/java/lk/ijse/pos_backend/dao/custom/impl/ItemDAOImpl.java
@@ -1,40 +1,69 @@
 package lk.ijse.pos_backend.dao.custom.impl;
 
 import lk.ijse.pos_backend.dao.custom.ItemDAO;
+import lk.ijse.pos_backend.dto.CustomerDTO;
 import lk.ijse.pos_backend.dto.ItemDTO;
 import lk.ijse.pos_backend.util.CrudUtil;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDAOImpl implements ItemDAO {
     @Override
     public List<ItemDTO> getAll() throws SQLException {
-        return List.of();
+        ResultSet rst = CrudUtil.crudUtil("SELECT * FROM Item");
+        List<ItemDTO> list = new ArrayList<>();
+        while (rst.next()){
+            list.add(new ItemDTO(
+                    rst.getString("id"),
+                    rst.getString("name"),
+                    rst.getDouble("price"),
+                    rst.getInt("qty")
+            ));
+        }
+        return list;
     }
 
     @Override
     public boolean save(ItemDTO dto) throws SQLException {
-        return CrudUtil.crudUtil("INSERT INTO Item (id,name,price,qty) VALUES (?,?,?,?)",dto.getId(),dto.getName(),dto.getAddress());
+        return CrudUtil.crudUtil("INSERT INTO Item (id,name,price,qty) VALUES (?,?,?,?)",dto.getId(),dto.getName(),dto.getPrice(),dto.getQty());
     }
 
     @Override
     public boolean update(ItemDTO dto) throws SQLException {
-        return false;
+        return CrudUtil.crudUtil("UPDATE Item SET name=?, price=?, qty=? WHERE id=?",dto.getName(),dto.getPrice(),dto.getQty(),dto.getId());
     }
 
     @Override
     public boolean delete(String id) throws SQLException {
-        return false;
+        return CrudUtil.crudUtil("DELETE FROM Item WHERE id=?",id);
     }
 
     @Override
     public String generateNextId() throws SQLException {
-        return "";
+        ResultSet rst = CrudUtil.crudUtil("SELECT id FROM Item ORDER BY id DESC LIMIT 1");
+        if (rst.next()) {
+            String id = rst.getString("id");
+            int newCustomerId = Integer.parseInt(id.replace("I00-", "")) + 1;
+            return String.format("I00-%03d", newCustomerId);
+        } else {
+            return "I00-001";
+        }
     }
 
     @Override
     public ItemDTO getById(String id) throws SQLException {
+        ResultSet rst = CrudUtil.crudUtil("SELECT * FROM Item WHERE id=?",id);
+        if(rst.next()){
+            return new ItemDTO(
+                    rst.getString("id"),
+                    rst.getString("name"),
+                    rst.getDouble("price"),
+                    rst.getInt("qty")
+            );
+        }
         return null;
     }
 }
--- src/main/java/lk/ijse/pos_backend/dto/ItemDTO.java
+++ src/main/java/lk/ijse/pos_backend/dto/ItemDTO.java
@@ -1,4 +1,15 @@
 package lk.ijse.pos_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDTO {
+    private String id;
+    private String name;
+    private double price;
+    private int qty;
 }
